5059a96ece165be7d9855d8fb6ff88cd1ab335c1
AzimJaan Akhtamov <azimaxtamov2oo2@mail.ru>
2023-10-01 14:17:17 +0500

    finished(Ad new product Screen >>>validations,form<<<<

--- kickio/lib/ui/screens/add_product_screen.dart
+++ kickio/lib/ui/screens/add_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kickio/data/models/product.module.dart';
 
 class AddProductScreen extends StatefulWidget {
   const AddProductScreen({super.key});
@@ -10,6 +11,16 @@ class AddProductScreen extends StatefulWidget {
 
 class _AddProductScreenState extends State<AddProductScreen> {
   final _priceFocus = FocusNode();
+  final _fromKey = GlobalKey<FormState>();
+  final _imgFormKey = GlobalKey<FormState>();
+
+  var _productDetails = ShoesProduct(
+    id: '',
+    title: '',
+    imgUrl: '',
+    description: '',
+    price: 0.0,
+  );
 
   @override
   void dispose() {
@@ -22,11 +33,28 @@ class _AddProductScreenState extends State<AddProductScreen> {
       context: context,
       builder: (ctx) => AlertDialog(
         content: Form(
+          key: _imgFormKey,
           child: TextFormField(
+             validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please,enter a image url address!';
+                    }  else if (value.startsWith('https://')) {
+                      return 'Please,enter a correct image url address!';
+                    }
+                  },
             decoration: const InputDecoration(
               hintText: 'Image Url https://...',
               border: OutlineInputBorder(),
             ),
+            onSaved: (newValue) {
+              _productDetails = ShoesProduct(
+                id: '',
+                title: _productDetails.title,
+                imgUrl: newValue!,
+                description: _productDetails.description,
+                price: _productDetails.price,
+              );
+            },
           ),
         ),
         actions: [
@@ -39,12 +67,32 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 backgroundColor: MaterialStatePropertyAll(
               Color(0xFF47E0A0),
             )),
-            onPressed: () {},
+            onPressed: imgUrlSave,
             child: const Text("Save"),
           ),
         ],
       ),
     );
+  }
+
+  void saveForm() {
+    bool isValid = _fromKey.currentState!.validate();
+    if (isValid) {
+      _fromKey.currentState!.save();
+      print(_productDetails.title);
+      print(_productDetails.price);
+      print(_productDetails.description);
+      print(_productDetails.imgUrl);
+    }
+  }
+
+  void imgUrlSave() {
+    bool isimgValid = _imgFormKey.currentState!.validate();
+   if(isimgValid){
+     _imgFormKey.currentState!.save();
+    setState(() {});
+    Navigator.of(context).pop();
+   }
   }
 
   @override
@@ -55,60 +103,119 @@ class _AddProductScreenState extends State<AddProductScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: saveForm,
             icon: const Icon(Icons.save),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
         child: Form(
+          key: _fromKey,
           child: SingleChildScrollView(
+            padding: const EdgeInsets.all(15),
             child: Column(
               children: [
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please,enter a product name!';
+                    }
+                    return null;
+                  },
                   onFieldSubmitted: (_) =>
                       FocusScope.of(context).requestFocus(_priceFocus),
                   decoration: const InputDecoration(
                     labelText: 'Name',
                     border: OutlineInputBorder(),
                   ),
+                  onSaved: (newValue) {
+                    _productDetails = ShoesProduct(
+                      id: '',
+                      title: newValue!,
+                      imgUrl: _productDetails.imgUrl,
+                      description: _productDetails.description,
+                      price: _productDetails.price,
+                    );
+                  },
                 ),
                 const SizedBox(height: 20),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please,enter a price!';
+                    } else if (double.tryParse(value) == null) {
+                      return 'Please,enter only numbers!';
+                    } else if (double.parse(value) <= 0) {
+                      return 'Please,enter a correct price!';
+                    }
+                  },
                   decoration: const InputDecoration(
                     labelText: 'Price',
                     border: OutlineInputBorder(),
                   ),
-                  focusNode: _priceFocus,
+                  // focusNode: _priceFocus,
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.next,
+                  onSaved: (newValue) {
+                    _productDetails = ShoesProduct(
+                      id: '',
+                      title: _productDetails.title,
+                      imgUrl: _productDetails.imgUrl,
+                      description: _productDetails.description,
+                      price: double.parse(newValue!),
+                    );
+                  },
                 ),
                 const SizedBox(height: 20),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please,enter a description!';
+                    } else if (value.length < 20) {
+                      return 'Please,enter at least 20 words!';
+                    }
+                  },
                   decoration: const InputDecoration(
                     labelText: 'Description',
                     alignLabelWithHint: true,
                     border: OutlineInputBorder(),
                   ),
                   maxLines: 4,
+                  onSaved: (newValue) {
+                    _productDetails = ShoesProduct(
+                      id: '',
+                      title: _productDetails.title,
+                      imgUrl: _productDetails.imgUrl,
+                      description: newValue!,
+                      price: _productDetails.price,
+                    );
+                  },
                 ),
                 const SizedBox(height: 20),
                 InkWell(
                   onTap: () => showUrlDialog(context),
                   borderRadius: BorderRadius.circular(8),
-                  child: const Card(
+                  child: Card(
                     margin: EdgeInsets.zero,
-                    shape: RoundedRectangleBorder(
+                    shape: const RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(8)),
                       side: BorderSide(color: Colors.grey, width: 1),
                     ),
                     child: SizedBox(
-                      width: double.infinity,
-                      height: 150,
-                      child: Center(
-                        child: Text('Paste Image Url'),
-                      ),
+                      width: MediaQuery.sizeOf(context).width * 0.7,
+                      height: 180,
+                      child: _productDetails.imgUrl.isEmpty
+                          ? const Center(
+                              child: Text('Paste Image Url'),
+                            )
+                          : ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(
+                                _productDetails.imgUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                     ),
                   ),
                 ),
